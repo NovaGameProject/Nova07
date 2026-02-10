6e7d25ddf468f02f6aaf59b53383d91359ec529a
brambora69123 <thenicolasdev@gmail.com>
2026-02-10 15:43:46 +0100

    Add license and license header.

--- src/Engine/Reflection/LevelLoader.cpp
+++ src/Engine/Reflection/LevelLoader.cpp
@@ -1,3 +1,11 @@
+// Nova Game Engine
+// Copyright (C) 2026  brambora69123
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
 #include "Engine/Reflection/InstanceFactory.hpp"
 #include "Engine/Reflection/LevelLoader.hpp"
 #include "Common/MathTypes.hpp"
