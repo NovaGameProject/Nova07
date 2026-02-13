63eaec8df02d6662cbe41a24f0dc3f0e81865476
brambora69123 <thenicolasdev@gmail.com>
2026-02-13 13:04:19 +0100

    add missing license headers

--- src/Engine/Window.hpp
+++ src/Engine/Window.hpp
@@ -1,3 +1,11 @@
+// Nova Game Engine
+// Copyright (C) 2026  brambora69123
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
 #pragma once
 #include "Engine/TaskScheduler.hpp"
 #include <SDL3/SDL.h>
